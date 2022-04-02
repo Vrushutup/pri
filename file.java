8330b78ec5b1690c25c115fbc1052d672dcaf90d
Vrushabh V Shamkuwar <vrushabhs1998@gmail.com>
2022-04-02 12:48:30 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,16 @@
+// import java.io.*;
+import java.util.*;
+
+public class file {
+public static void solution(String x){
+System.out.println(x);
+return;
+}
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    // int n=scn.nextInt();
+    // String s=scn.next();
+    solution(s);
+    }
+    
+}
